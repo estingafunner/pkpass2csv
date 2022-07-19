3b8e003b975a9cb6fd6a7571b5b52e13e9dd318a
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-07-19 09:19:17 -0700

    Using Keyd system
    create keyd files for each orgName(airline), write function which imports variables based on matching keyd file.

--- secondTry.py
+++ secondTry.py
@@ -1,11 +1,8 @@
+import pandas as pd
 from zipfile import ZipFile
-import uuid
 import json
 import os
 import shutil
-import tkinter as tk
-import tkinter.filedialog as fdi
-import pandas as pd
 
 
 def json2csv(passJson):
@@ -13,33 +10,24 @@ def json2csv(passJson):
         data = json.load(read_file)
 
     df = pd.json_normalize(data)
-    df.to_csv(r'outputFile.csv', index = None) #NEED TO CREATE A NEW CSV FOR EVERY JSON
+    df.to_csv(r'outputFile.csv', index = None)
 
     #print(data)
     print("wat?")
 
 def unzipper():
     # assign directory
-
-    root = tk.Tk()
-    filez = fdi.askopenfilenames(parent=root, title='Choose a file')
-    print(filez)
-
-    """     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    fn = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-    print(fn) """
-
-    #dir1 = 'pkpass' - replaced with user prompt
+    dir1 = 'pkpass'
     dir2 = 'passes'
     dir3 = 'unzipped'
     j = 0
 
-    for filename in filez:
-        #f = os.path.join(dir1, filename)
+    for filename in os.listdir(dir1):
+        f = os.path.join(dir1, filename)
 
-        if os.path.isfile(filename):
+        if os.path.isfile(f):
 
-            with ZipFile(filename, 'r') as zipObject:
+            with ZipFile(f, 'r') as zipObject:
                 listOfFileNames = zipObject.namelist()
 
                 for passName in listOfFileNames:
@@ -47,9 +35,8 @@ def unzipper():
                     if passName == "pass.json":
                         j += 1
                         zipObject.extract(passName, dir3)
-                        uuidPass = (str(uuid.uuid1())[0:14]) + ".json"
-                        shutil.move((dir3 + "/" + passName), (dir2 + "/" + uuidPass))#str(j) + passName)) 
-                        thisFile = dir2 + "/" + uuidPass
-                        json2csv(thisFile) 
+                        shutil.move((dir3 + "/" + passName), (dir2 + "/" + str(j) + passName)) 
+                        thisFile = dir2 + "/" + str(j) + passName
+                        #json2csv(thisFile) 
 
 unzipper()
