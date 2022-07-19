980b3b4e164be7566d8f6d1486c734ef3e0bdec3
Marcus Larson <38468418+estingafunner@users.noreply.github.com>
2022-07-19 15:04:09 -0700

    push to desktop
    READ the notes.
    THEN address them point by point

--- secondTry.py
+++ secondTry.py
@@ -1,4 +1,4 @@
-import pandas as pd
+/import pandas as pd
 from zipfile import ZipFile
 import json
 import os
@@ -8,12 +8,39 @@ import shutil
 def json2csv(passJson):
     with open(passJson, "r") as read_file:
         data = json.load(read_file)
-
     df = pd.json_normalize(data)
     df.to_csv(r'outputFile.csv', index = None)
 
     #print(data)
     print("wat?")
+
+def keyd(passJson2):
+    with open(passJson2, "r") as read_file:
+        data = json.load(read_file)
+    df = pd.json_normalize(data)
+
+    #open pass.jsom file DONE
+    #open keys.csv file
+    #open outputFile.csv file
+    #read the value of organizationName in pass.json
+    #find match for organizationName.value in first column of keys.csv
+    ##new direction:
+        #if organizationName == airlilne
+            #for each col in row(orgName's row)
+                #input (col,row).val into outputfile.csv @ lastRow in same col
+            
+        #save outputFile.csv
+        #save outputFile.csv
+        #close keys.csv
+        ####### go to NEXT pkpass file @ unzipper() ::::: update unzipper() to accept multiple or single files
+
+    #assign variables to each value in corresponding row
+    #add those values to outputFIle.csv (this may be a seperate fuction, OR 
+    #this could be greatly abbreviated by nesting the addition of data to the csv directly from the keys.csv
+    #skip the step where variables get defined: for each row in col, add to csv @ new line (enumerate?, maybe go to list, then convert to csv? maybe easier to manage? research)
+
+    #CONVERT json
+    #json2csv(thisFile) - useless to this function
 
 def unzipper():
     # assign directory
@@ -37,6 +64,10 @@ def unzipper():
                         zipObject.extract(passName, dir3)
                         shutil.move((dir3 + "/" + passName), (dir2 + "/" + str(j) + passName)) 
                         thisFile = dir2 + "/" + str(j) + passName
-                        json2csv(thisFile) 
+
+                        #HERE! 12:49 19-Jul-2022
+                        #keyd(thisFile)
+:                       
+                        #json2csv(thisFile) - probably done with that? may just use later?
 
 unzipper()
